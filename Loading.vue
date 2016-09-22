e560f922a36721872723423a5a811a3391c97a82
Igor De Paula <principe.borodin@gmail.com>
2016-09-22 16:16:30 -0300

    Initial

---
+++ Loading.vue
@@ -0,0 +1,20 @@
+<script>
+    import NProgress from 'nprogress';
+    const install = (VueInstance) =
+    >
+    {
+        const $loader = {
+            show() {
+                NProgress.start();
+            },
+            hide() {
+                NProgress.done();
+            },
+        };
+
+        VueInstance.prototype.$loader = $loader;
+    }
+    ;
+
+    export default {install};
+</script>
---
+++ Readme.md
@@ -0,0 +1,25 @@
+Vue Loading Ajax NProgress
+==========================
+
+This is a plugin for add nprogress on all ajax calls from vuejs.
+
+
+On your app.js
+```javascript
+import Loading from 'Loading'
+
+Vue.use(Loading);
+
+```
+
+On your components that call ajax.
+
+```javascript
+
+//On your method
+//to show
+this.$loader.show();
+//to hide
+this.$loader.show();
+
+```
